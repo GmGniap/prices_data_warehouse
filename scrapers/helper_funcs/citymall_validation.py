c5fcab24ad2664d25c7e21c8dac09775bd485215
gmgniap <thetpaing@utycc.edu.mm>
2024-03-16 02:16:30 +0630

    CityMall Validation

---
+++ scrapers/helper_funcs/citymall_validation.py
@@ -0,0 +1,21 @@
+## Do I need to write custom error for not able to access URL with requests
+
+import re
+class CityMallInputValidation:
+    '''
+    To validate user input data to be correct form.
+    '''
+    
+    def validate_category_url(self, input_category_url):
+        ## Check w/ Regex
+        
+        ## Check it's existing/correct URL
+        ## Need of std list of all available urls
+        ## if it's correct , can return matched category name
+        pass
+    
+    def validate_available_category_name(self):
+        ## category started with MU or SU can't be scraped.
+        pass
+        
+        
